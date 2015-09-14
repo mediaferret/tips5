c8a787ce2d14ea7c7cc9dfc774a4e02d6854d0b5
Kevin O'Connell <koconnell@tcc-koconnell.local>
2015-09-14 14:26:17 -0700

    Initial Commit

---
+++ tips5Tests/tips5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tips5Tests.swift
+//  tips5Tests
+//
+//  Created by Kevin O'Connell on 9/14/15.
+//  Copyright © 2015 Kevin O'Connell. All rights reserved.
+//
+
+import XCTest
+@testable import tips5
+
+class tips5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
